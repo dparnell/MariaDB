592bf36b41ba30ae784de4d9fb202c20f57216e7
Daniel Parnell <me@danielparnell.com>
2012-12-05 20:53:07 +1100

    Initial Commit

---
+++ MariaDB/MariaDB.h
@@ -0,0 +1,13 @@
+//
+//  MariaDB.h
+//  MariaDB
+//
+//  Created by Daniel Parnell on 5/12/12.
+//  Copyright (c) 2012 Automagic Software Pty Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MariaDB : NSObject
+
+@end
---
+++ MariaDB/MariaDB.m
@@ -0,0 +1,13 @@
+//
+//  MariaDB.m
+//  MariaDB
+//
+//  Created by Daniel Parnell on 5/12/12.
+//  Copyright (c) 2012 Automagic Software Pty Ltd. All rights reserved.
+//
+
+#import "MariaDB.h"
+
+@implementation MariaDB
+
+@end
